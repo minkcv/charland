ae255a1c6f5cecc445ca5427b3b675489f4def5c
Will Smith <minkcv@gmail.com>
2017-07-10 08:00:09 -0600

    change tabs to spaces $$$

--- game.js
+++ game.js
@@ -17,9 +17,9 @@ var tileHeight = dummyPre.getBoundingClientRect().height;
 var divcols = 64;
 var divrows = 64;
 if (worldLines.length % divrows != 0)
-	console.log("number of lines in the world must be a multiple of divrows (" + divrows + ")");
+    console.log("number of lines in the world must be a multiple of divrows (" + divrows + ")");
 if (worldLines[0].length % divrows != 0)
-	console.log("number of characters in a world line should be a multiple of divcols (" + divcols + ")");
+    console.log("number of characters in a world line should be a multiple of divcols (" + divcols + ")");
 var worldData = [];
 var worldDivs = [];
 var walls = [];
@@ -27,44 +27,44 @@ var gamestart = true;
 var playerstartx = 220 * tileWidth;
 var playerstarty = 110 * tileHeight;
 for (k = 0; k < worldLines.length / divrows; k++) {
-	worldData[k] = [];
-	for (i = 0; i < worldLines[k].length / divcols; i++) {
-		var chunk = "";
-		for (j = 0; j < divrows; j++) {
-			chunk += worldLines[(k * divrows) + j].substring(i * divcols, (i + 1) * divcols) + "\n";
-		}
-		worldData[k][i] = chunk;
-	}
+    worldData[k] = [];
+    for (i = 0; i < worldLines[k].length / divcols; i++) {
+        var chunk = "";
+        for (j = 0; j < divrows; j++) {
+            chunk += worldLines[(k * divrows) + j].substring(i * divcols, (i + 1) * divcols) + "\n";
+        }
+        worldData[k][i] = chunk;
+    }
 }
 for (j = 0; j < worldData.length; j++) {
-	worldDivs[j] = [];
-	for (i = 0; i < worldData[j].length; i++) {
-		var d = document.createElement("pre");
-		d.style.position = "absolute";
-		d.style.position = "absolute";
-		d.style.fontFamily = "monospace";
-		d.style.color = "white";
-		d.style.margin = 0;
-		d.style.left = i * divcols * tileWidth;
-		d.style.top = j * divrows * tileHeight;
-		var txt = document.createTextNode(worldData[j][i]);
-		d.appendChild(txt);
-		document.body.appendChild(d);
-		worldDivs[j][i] = d;
-	}
+    worldDivs[j] = [];
+    for (i = 0; i < worldData[j].length; i++) {
+        var d = document.createElement("pre");
+        d.style.position = "absolute";
+        d.style.position = "absolute";
+        d.style.fontFamily = "monospace";
+        d.style.color = "white";
+        d.style.margin = 0;
+        d.style.left = i * divcols * tileWidth;
+        d.style.top = j * divrows * tileHeight;
+        var txt = document.createTextNode(worldData[j][i]);
+        d.appendChild(txt);
+        document.body.appendChild(d);
+        worldDivs[j][i] = d;
+    }
 }
 
 for (i = 0; i < worldLines.length; i++) {
-	walls[i] = [];
-	for (j = 0; j < worldLines[i].length; j++) {
-		if (worldLines[i][j] != ' ')
-			walls[i][j] = { 
-				left: j * tileWidth,
-				right: (j + 1) * tileWidth,
-				top: i * tileHeight,
-				bottom: (i + 1) * tileHeight
+    walls[i] = [];
+    for (j = 0; j < worldLines[i].length; j++) {
+        if (worldLines[i][j] != ' ')
+            walls[i][j] = { 
+                left: j * tileWidth,
+                right: (j + 1) * tileWidth,
+                top: i * tileHeight,
+                bottom: (i + 1) * tileHeight
             };
-	}
+    }
 }
 
 document.body.style.backgroundColor = "black";
@@ -91,44 +91,44 @@ var movespeed = 3;
 
 // Rectangle intersection check.
 function intersects(r1, r2) {
-	return !(r2.left >= r1.right ||
-		 r2.right <= r1.left ||
-		 r2.top >= r1.bottom ||
-		 r2.bottom <= r1.top);
+    return !(r2.left >= r1.right ||
+        r2.right <= r1.left ||
+        r2.top >= r1.bottom ||
+        r2.bottom <= r1.top);
 }
 
 function collisionDetection() {
-	// Player collision rectangles.
-	var leftRect = { left: playerx + playerxv, 
-			 right: playerx + playerw + playerxv, 
-			 top: playery, 
-			 bottom: playery + playerh };
-	var rightRect = { left: playerx + playerw, 
-			 right: playerx + playerw + playerxv, 
-			 top: playery, 	
-			 bottom: playery + playerh };
-	var topRect = { left: playerx, 
-			right: playerx + playerw, 
-			top: playery + playeryv, 
-			bottom: playery };
-	var botRect = { left: playerx, 
-			right: playerx + playerw, 
-			top: playery + playerh, 
-			bottom: playery + playerh + playeryv };
+    // Player collision rectangles.
+    var leftRect = { left: playerx + playerxv, 
+            right: playerx + playerw + playerxv, 
+            top: playery, 
+            bottom: playery + playerh };
+    var rightRect = { left: playerx + playerw, 
+            right: playerx + playerw + playerxv, 
+            top: playery, 
+            bottom: playery + playerh };
+    var topRect = { left: playerx, 
+            right: playerx + playerw, 
+            top: playery + playeryv, 
+            bottom: playery };
+    var botRect = { left: playerx, 
+            right: playerx + playerw, 
+            top: playery + playerh, 
+            bottom: playery + playerh + playeryv };
 
-	var topleftRect = { left: playerx + playerxv, 
+    var topleftRect = { left: playerx + playerxv, 
                         right: playerx + playerw + playerxv, 
                         top: playery + playeryv, 
                         bottom: playery };
-	var botleftRect = { left: playerx + playerxv,
+    var botleftRect = { left: playerx + playerxv,
                         right: playerx + playerw + playerxv,
                         top: playery + playerh,
                         bottom: playery + playerh + playeryv };
-	var toprightRect = { left: playerx + playerw, 
+    var toprightRect = { left: playerx + playerw, 
                          right: playerx + playerw + playerxv, 
                          top: playery + playeryv, 
                          bottom: playery };
-	var botrightRect = { left: playerx + playerw,
+    var botrightRect = { left: playerx + playerw,
                          right: playerx + playerw + playerxv, 
                          top: playery + playerh, 
                          bottom: playery + playerh + playeryv };
@@ -138,10 +138,10 @@ function collisionDetection() {
     var topIntersects = false;
     var botIntersects = false;
 
-	for (i = 0; i < walls.length; i++) {
-		for (j = 0; j < walls[i].length; j++) {
-			if (walls[i][j]) {
-				if (intersects(leftRect, walls[i][j]) && playerxv < 0)
+    for (i = 0; i < walls.length; i++) {
+        for (j = 0; j < walls[i].length; j++) {
+            if (walls[i][j]) {
+                if (intersects(leftRect, walls[i][j]) && playerxv < 0)
                 {
                     var leftRem = playerx - walls[i][j].right;
                     playerxv = -Math.floor(leftRem);
@@ -150,29 +150,29 @@ function collisionDetection() {
                 if (intersects(topRect, walls[i][j]) && playeryv < 0)
                 {
                     var topRem = playery - walls[i][j].bottom;
-					playeryv = -Math.floor(topRem);
+                    playeryv = -Math.floor(topRem);
                     topIntersects = true;
                 }
 
                 if (intersects(rightRect, walls[i][j]) && playerxv > 0)
                 {
                     var rightRem = walls[i][j].left - (playerx + playerw);
-					playerxv = Math.floor(rightRem);
+                    playerxv = Math.floor(rightRem);
                     rightIntersects = true;
                 }
-				if (intersects(botRect, walls[i][j]) && playeryv > 0)
+                if (intersects(botRect, walls[i][j]) && playeryv > 0)
                 {
                     var botRem = walls[i][j].top - (playery + playerh);
-					playeryv = Math.floor(botRem);
+                    playeryv = Math.floor(botRem);
                     botIntersects = true;
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 
-	for (i = 0; i < walls.length; i++) {
-		for (j = 0; j < walls[i].length; j++) {
-			if (walls[i][j]) {
+    for (i = 0; i < walls.length; i++) {
+        for (j = 0; j < walls[i].length; j++) {
+            if (walls[i][j]) {
                 if (!leftIntersects && !topIntersects)
                 {
                     if (intersects(topleftRect, walls[i][j]) && playerxv < 0 && playeryv < 0)
@@ -207,11 +207,11 @@ function collisionDetection() {
 }
 
 addEventListener("keydown", function(e) {
-	keysDown[e.keyCode] = true;
+    keysDown[e.keyCode] = true;
 }, false);
 
 addEventListener("keyup", function(e) {
-	delete keysDown[e.keyCode];
+    delete keysDown[e.keyCode];
 }, false);
 
 // This has gotten so hacky but it's beautiful how well this handles resize.
@@ -221,25 +221,25 @@ addEventListener("resize", function() {
 });
 
 setInterval(function() {
-	if (38 in keysDown) { // UP
-		playeryv = -movespeed;
-	}
-	else if (40 in keysDown) { // DOWN
-		playeryv = movespeed;
-	}
-	else
-		playeryv = 0;
-	if (39 in keysDown) { // RIGHT
-		playerxv = movespeed;
-	}
-	else if (37 in keysDown) { // LEFT
-		playerxv = -movespeed;
-	}
-	else
-		playerxv = 0;
+    if (38 in keysDown) { // UP
+        playeryv = -movespeed;
+    }
+    else if (40 in keysDown) { // DOWN
+        playeryv = movespeed;
+    }
+    else
+        playeryv = 0;
+    if (39 in keysDown) { // RIGHT
+        playerxv = movespeed;
+    }
+    else if (37 in keysDown) { // LEFT
+        playerxv = -movespeed;
+    }
+    else
+        playerxv = 0;
 
-	// Yay, side effects on global variables.
-	collisionDetection();
+    // Yay, side effects on global variables.
+    collisionDetection();
 
     // Yet another hack because I can't just set playerx or playery
     if (gamestart)
@@ -249,13 +249,13 @@ setInterval(function() {
         gamestart = false;
     }
 
-	playerx += playerxv;
-	playery += playeryv;
-	for (j = 0; j < worldDivs.length; j++) {
-		for (i = 0; i < worldDivs[j].length; i++) {
-			var d = worldDivs[j][i];
-			d.style.left = -playerxv + parseInt(d.style.left);
-			d.style.top = -playeryv + parseInt(d.style.top);
-		}
-	}
+    playerx += playerxv;
+    playery += playeryv;
+    for (j = 0; j < worldDivs.length; j++) {
+        for (i = 0; i < worldDivs[j].length; i++) {
+            var d = worldDivs[j][i];
+            d.style.left = -playerxv + parseInt(d.style.left);
+            d.style.top = -playeryv + parseInt(d.style.top);
+        }
+    }
 }, 20);
